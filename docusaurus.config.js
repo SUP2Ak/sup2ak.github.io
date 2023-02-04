5ff1b89178e4ed39e8f5ed92268b097bfc77374d
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-02-04 02:08:41 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,18 +9,15 @@ const config = {
   title: 'SUP2Ak',
   tagline: 'Developement is cool',
   favicon: 'img/favicon.ico',
-
   // Set the production url of your site here
   url: 'https://sup2ak.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sup2ak', // Usually your GitHub org/user name.
-  projectName: 'supv', // Usually your repo name.
-
+  projectName: 'sup2ak.github.io', // Usually your repo name. organizationName: 'overextended', // Usually your GitHub org/user name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -37,13 +34,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        /*fivem: {
-          sidebarPath: require.resolve('./sidebarsfivem.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/',
-        },*/
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
@@ -51,14 +41,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        /*fivem: {
-          showReadingTime: true,
-          //sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },*/
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
