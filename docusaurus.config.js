91be29665f788cb25fe0e66d2073288158fbcd6c
SUP2Ak <31973315+SUP2Ak@users.noreply.github.com>
2023-04-23 01:37:32 +0000

    sd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   projectName: 'sup2ak.github.io', // Usually your repo name. organizationName: 'overextended', // Usually your GitHub org/user name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'main',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
