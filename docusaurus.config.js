9a85e05e9630de99184d38697d03d431944b319f
SUP2Ak <cormier.wesley@gmail.com>
2023-04-29 01:19:20 +0200

    sd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   projectName: 'sup2ak.github.io', // Usually your repo name. organizationName: 'overextended', // Usually your GitHub org/user name.
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
