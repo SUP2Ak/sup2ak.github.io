0c0afc8313a0017d13c8759aed21ed072c5d4763
SUP2Ak <cormier.wesley@gmail.com>
2023-04-29 03:27:57 +0200

    Deploy website - based on 1152a9f2a4e1d203cee48a18f916c77193d4cf28

---
+++ assets/js/6c2ac815.58e03bde.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[560],{6087:e=>{e.exports=JSON.parse('{"title":"supv_core","description":"Une documention en plus \ud83d\udc40","slug":"/category/supv_core","permalink":"/docs/category/supv_core","navigation":{"next":{"title":"Utilisation","permalink":"/docs/supv_core/"}}}')}}]);
